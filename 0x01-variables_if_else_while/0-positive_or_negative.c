e403783392ad5cbfa558458d4341440cb9da66c8
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-09 16:08:08 -0700

    random +-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,6 +14,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	(n > 0) ? printf("is postive\n") : (n < 0) ? printf("is negative\n") : printf("is zero\n");
+	(n > 0) ? printf("%d is positive\n", n) : (n < 0) ? printf("%d is negative\n", n) : printf("%d is zero\n", n);
 	return (0);
 }
