58ef30bd8d4f2506a35808cffbeaa0b9f86b383d
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-08 18:57:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
 int main(void)
 
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/* https://stackoverflow.com/questions/25411644/printing-double-quote-in-c */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
