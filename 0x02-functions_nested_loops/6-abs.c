d626a7de5b09145fb0d5e2e5baa6768f482f210b
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-13 15:58:34 -0700

    return abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - return the absolute value of a given number
+ *
+ * @n: int to be processed
+ *
+ * Return: int abs
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
