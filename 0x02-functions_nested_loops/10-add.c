18563ba521ad0c653152ee739d30b4e0803140bf
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-13 16:50:49 -0700

    add two passed nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add two integers
+ *
+ * @a: first passed integer
+ * @b: second passed integer
+ *
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
