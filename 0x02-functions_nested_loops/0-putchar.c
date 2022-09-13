93c7f772baf4bee29aa45f0304a8b9781e116648
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-13 05:24:05 -0700

    print string

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  *
  * description: 'program that prints _putchar, followed by a new line.'
  *
- * Return: Always 0 (Success)
+ * Retur: Always 0 (Success)
  */
 int main(void)
 {
