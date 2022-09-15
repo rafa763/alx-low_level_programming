2dcefc169d067daf38ca8f429f0a8a61882c1910
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-15 14:38:32 -0700

    print 0~9 except 2,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - print from 0~9 except 2 and 4
+ *
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
