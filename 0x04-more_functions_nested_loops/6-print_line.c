fb885c65298ff4c3690a655c6501c15093b9aa71
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-15 15:53:08 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - print line of length n
+ *
+ * @n: first integer
+ *
+ * Return: Void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
