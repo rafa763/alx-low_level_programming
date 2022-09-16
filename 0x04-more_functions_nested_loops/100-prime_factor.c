6825abb2d9f85f89ea0049731dc7997a6d018f5d
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-15 19:30:48 -0700

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <math.h>
-
+/**
+ * main - entry point
+ * description: 'print largest prime factor number'
+ * Return: void
+ */
 int main(void)
 {
 	long int n = 612852475143, i, c = 0;
 
 	while (n % 2 == 0)
 	{
-		n = n/2;
+		n = n / 2;
 		c = 2;
 	}
 
