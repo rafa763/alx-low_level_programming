3ce197e21d6b261d2564e3bb2a6db7f5a5db39c4
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-22 19:02:46 -0700

    capitalize all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *cap_string - capitalize all words of a string
+ *
+ * @c: string to be converted to capped
+ *
+ * Return: Char
+ */
+char *cap_string(char *c)
+{
+	int i, j;
+	char ex[] = {
+		' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'
+	};
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; ex[j] != '\0'; j++)
+		{
+			if (c[i] == ex[j])
+			{
+				if (c[i + 1] >= 97 && c[i + 1] <= 122)
+					c[i + 1] -= 32;
+			}
+		}
+	}
+	return (c);
+}
