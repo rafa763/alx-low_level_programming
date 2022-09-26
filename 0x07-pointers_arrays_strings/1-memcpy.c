5930bdcc52c4a16aaf7743443a445f25c081ae86
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-26 15:58:38 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_memcpy - copy memory data
+ *
+ * @dest: destination to be copied to
+ * @src: source to copy from
+ *@n: number of bytes
+ *
+ * Return: *dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (dest);
+}
