14a5fcd2d3fba9ace1d7727d5a2af101ab6f6562
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-26 15:50:34 -0700

    fill memeory with constant bytes

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_memset - fill the first n bytes of memory pointed to by s with byte b
+ *
+ * @s: pointer to the written memory
+ * @b: char to be written
+ *@n: number of bytes
+ *
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(char *s, char b, unsigned int n);
+}
