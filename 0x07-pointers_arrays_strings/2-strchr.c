d835cd7f67d6e46691236975bd1484b39de454d1
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-09-26 16:23:27 -0700

    locate char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strchr - locates a character in a string
+ *
+ * @s: string
+ * @c: char to be found
+ *
+ * Return: *s
+ */
+char *_strchr(char *s, char c)
+{
+	char* ch = strchr(s, c);
+	return (ch);
+}
