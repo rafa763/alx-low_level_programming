ea63075162d35053d29b9f601b649bca27046fd1
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-09 21:56:23 -0700

    update headers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,9 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
+char *string_toupper(char *c);
+char *cap_string(char *c);
+char *leet(char *c);
+char *rot13(char *);
 
 #endif /* MAIN_H */
