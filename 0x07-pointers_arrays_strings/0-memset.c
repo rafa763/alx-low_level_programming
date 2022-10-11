791567aafd294a88ce8bf8300c2cd9435fbfa23a
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-11 14:38:49 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,21 @@
 #include "main.h"
-#include <string.h>
 /**
  * *_memset - fill the first n bytes of memory pointed to by s with byte b
  *
  * @s: pointer to the written memory
  * @b: char to be written
- *@n: number of bytes
+ * @n: number of bytes
  *
  * Return: *s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	memset(s, b, n);
-	return (s);
+	char *result = s;
+
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (result);
 }
