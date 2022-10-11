47e01b16c632e0e9d287bfba903db00704b0e5e1
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-11 15:38:01 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * *_strchr - locates a character in a string
  *
@@ -10,8 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *ch;
 
-	ch = strchr(s, c);
-	return (ch);
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
 }
