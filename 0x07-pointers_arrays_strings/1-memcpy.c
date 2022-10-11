fda24b27fc0dd87cfed7878798aa823b2183df15
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-11 15:05:29 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,18 @@
 #include "main.h"
-#include <string.h>
 /**
  * *_memcpy - copy memory data
  *
  * @dest: destination to be copied to
  * @src: source to copy from
- *@n: number of bytes
+ * @n: number of bytes
  *
  * Return: *dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest, src, n);
-	return (dest);
+	char *result = dest;
+
+	while (n--)
+		*dest++ = *src++;
+	return (result);
 }
