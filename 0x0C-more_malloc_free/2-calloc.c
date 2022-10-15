199619859f5354e085f1009e391e03cc1c0e4a7d
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-15 12:06:36 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,14 +10,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	char *ptr;
+	unsigned int i = 0;
 
 	if (!nmemb || !size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
-	while (*ptr)
-		*ptr++ = 0;
+	while (i < nmemb)
+	{
+		ptr[i] = 0;
+		i++;
+	}
 	return (ptr);
 }
