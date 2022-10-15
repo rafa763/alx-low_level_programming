fce7fc6127fba66ad21c877b9f98a3d82ca986b4
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-14 21:11:06 -0700

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,13 +12,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr, *str;
-	int size = 0;
+	int size1 = 0, size2 = 0;
 
 	(!s1) ? s1 = "" : 0;
 	(!s2) ? s2 = "" : 0;
-	while (s1[size])
-		size++;
-	ptr = (char *)malloc((size + n) * sizeof(char));
+	while (s1[size1])
+		size1++;
+	while (s2[size2])
+		size2++;
+	ptr = (char *)malloc((size1 + size2) * sizeof(char));
 	if (!ptr)
 		return (NULL);
 	str = ptr;
@@ -26,5 +28,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		*ptr++ = *s1++;
 	while (*s2 && n--)
 		*ptr++ = *s2++;
+	ptr = '\0';
 	return (str);
 }
