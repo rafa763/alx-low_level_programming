893fff99cc141b649bd37ba2d866da4e24bbb14a
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-21 14:51:12 -0700

    pass name to function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ *
+ * @name: name to be printed
+ * @f: pointer to a function
+ *
+ * Return: Void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
