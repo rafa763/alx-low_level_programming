c4721885e82647cda9789dbc8fba313078d19895
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-21 18:12:25 -0700

    print in passed mode

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-
+	
 	for (i = 0; i < size; i++)
 	{
-		if (*action && *array)
+		if (*action && array)
 			action(*array++);
 	}
 }
