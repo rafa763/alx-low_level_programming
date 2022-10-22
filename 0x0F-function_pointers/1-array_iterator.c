c172990892d057553fae0e7f2124508398437ab4
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-21 18:06:26 -0700

    print in passed mode

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - print array elemnts in specific format
+ *
+ * @array: pointer to first element in array
+ * @size: length of the array
+ * @action: pointer to the print function
+ *
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (*action)
+			action(*array++);
+	}
+}
