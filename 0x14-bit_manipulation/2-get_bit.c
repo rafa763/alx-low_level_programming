3818c9a7d3175ddd8417fa8406b08978a4bf98ea
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-27 17:39:34 -0700

    locate bit at specefic index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit - return value of a bit at a given index
+ *
+ * @n: given bit
+ * @index: bit to be found
+ *
+ * Return: int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (index--)
+		n >>= 1;
+	if (n & 1)
+		return (1);
+	else
+		return (0);
+}
