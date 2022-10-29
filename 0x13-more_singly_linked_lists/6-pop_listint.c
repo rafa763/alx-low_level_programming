faae0e04ef41984f85a3c9a5f5139036e9e15d23
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-28 20:54:21 -0700

    remove index

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - delete the head of a node
+ *
+ * @head: pointer to the head pointer
+ *
+ * Return: int inside the node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int num = 0;
+
+	if (head == NULL)
+		return (0);
+	ptr = *head;
+	num = ptr->n;
+	ptr = ptr->next;
+	*head = ptr;
+	return (num);
+}
