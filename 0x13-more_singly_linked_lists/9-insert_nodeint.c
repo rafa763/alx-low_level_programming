f25467fd1045e40ab90ba2599404783fdb05a745
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-29 15:44:41 -0700

    insert at specefic position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	ptr = *head;
+	if (idx == 1 || *head == NULL)
+	{
+		temp->n = n;
+		temp->next = *head;
+		*head = temp;
+		return (*head);
+	}
 	while (idx != 1 && ptr && idx--)
 		ptr = ptr->next;
 	if (ptr == NULL)
