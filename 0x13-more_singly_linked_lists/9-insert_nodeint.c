20ff93124e74831ddfe5e43bcd9fa755a052c958
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-10-29 19:18:40 -0700

    insert at n position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = temp;
 		return (*head);
 	}
-	while (idx != 2 && ptr)
+	while (idx != 1 && ptr)
 	{
 		ptr = ptr->next;
 		idx--;
