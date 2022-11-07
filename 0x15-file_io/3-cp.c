84a61fd3888ee8bd3fa75a62241026b8398eaad5
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-11-06 23:16:18 -0800

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,42 +14,52 @@
  */
 int main(int argc, char **argv)
 {
-	int fd1, fd2, stat1, stat2;
+	int fd1, fd2, stat1, stat2, c1, c2;
 	char *buffer;
 
 	if (argc < 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		return (97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	fd1 = open(argv[1], O_RDWR, 0664);
 	if (fd1 == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	fd2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		return (99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	buffer = malloc(sizeof(char) * 1024);
 	while ((stat1 = read(fd1, buffer, 1024)) > 0)
 	{
 		if (stat1 == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			return (98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
 		stat2 = write(fd2, buffer, stat1);
 		if (stat2 == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			return (99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
-	close(fd1);
-	close(fd2);
-	return (0);
+	c1 = close(fd1);
+	if (c1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1);
+		exit(100);
+	}
+	c2 = close(fd2);
+	if (c2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", c1);
+		exit(100);
+	}
+	exit(0);
 }
