85f820350246c1ce593f454e1e4367c36ea44d60
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-11-06 18:23:34 -0800

    append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 	{
-		if (fd == 0)
+		if (access(filename, F_OK) == 0)
 			return (1);
 		else
 			return (-1);
