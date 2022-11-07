d817e6da46b1c13e732516212bfd97b4cdbcc745
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2022-11-06 16:23:39 -0800

    write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 	{
-		open(filename, O_CREAT);
+		open(filename, O_CREAT, 0600);
 		return (1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[count])
