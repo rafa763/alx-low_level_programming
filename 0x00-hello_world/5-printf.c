3866763965fbb51b439ad8bbd7ac40b85eb27b21
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-09-20 04:23:26 +0200

    print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: print the string s using printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *s = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", s);
+	return (0);
+}
