7732a135d718f597aed691423a45fa7b15241632
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-09-20 04:18:35 +0200

    print without printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: print the string s without using printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *s = "\"Programming is like building a multilingual puzzle";
+
+	puts(s);
+	return (0);
+}
