9e8a90c86bd06bc035c3bcf14095135029481734
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-10-08 00:50:51 +0200

    advanced

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,14 +15,14 @@ int main(int argc, char *argv[])
 	int cents, t = 0, i = 0;
 	int change[] = {25, 10, 5, 2, 1};
 
-	cents = atoi(argv[1]);
-	if (argc != 2 || cents < 0)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	while (cents != 0)
+	cents = atoi(argv[1]);
+	while (cents >= 1)
 	{
 		if (cents >= change[i])
 		{
