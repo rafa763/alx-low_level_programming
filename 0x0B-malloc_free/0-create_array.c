ae1f6a5c270af9b8c26866905a02428daf7f1f15
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-10-10 20:52:45 +0200

    dynamic array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	p = malloc(sizeof(int) * size);
+	p = malloc(sizeof(char) * size);
 
 	if (!p)
 		return (NULL);
