ea680e1f1d83a34114e0dee3b2d263104d02c15b
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-10-11 08:50:02 +0200

    2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int **)malloc(sizeof(int *) * height);
+	arr = (int **)malloc(sizeof(int) * height);
 
 	if (!arr)
 		return (NULL);
