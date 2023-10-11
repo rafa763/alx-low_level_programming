a71e5925c709a57e022df3a039d50e95da6c43f1
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-10-11 22:17:07 +0200

    dynamic struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,20 +12,42 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *n, *o;
+	int s1 = 0, s2 = 0;
 	dog_t *dog;
 
 	dog = (dog_t *)malloc(sizeof(dog_t));
 	if (!dog)
+		return (NULL);
+
+	while (name[s1])
+		s1++;
+	while (owner[s2])
+		s2++;
+
+	n = (char *)malloc(sizeof(char) * (s1 + 1));
+	if (!n)
 	{
 		free(dog);
 		return (NULL);
 	}
+	dog->name = n;
 
-	n = name;
-	o = owner;
-	dog->name = name;
+	o = (char *)malloc(sizeof(char) * (s2 + 1));
+	if (!o)
+	{
+		free(o);
+		free(dog->name);
+		return (NULL);
+	}
+	dog->owner = o;
 	dog->age = age;
-	dog->owner = owner;
+
+	while (*owner)
+		*o++ = *owner++;
+	*o = '\0';
+	while (*name)
+		*n++ = *name++;
+	*n = '\0';
 
 	return (dog);
 }
