9c0b8c7f5920d8b5460f72ab6bedab58f911d098
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-10-25 04:57:16 +0200

    reverse linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -21,8 +21,10 @@ listint_t *reverse_listint(listint_t **head)
 		p2 = (*head)->next;
 		(*head)->next = p1;
 		p1 = *head;
-		(*head) = p2;
+		*head = p2;
 	}
+
+	*head = p1;
 
 	return (*head);
 }
